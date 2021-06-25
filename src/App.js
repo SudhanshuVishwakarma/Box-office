a6d2d654fee4df0ecfec293e5c11aed2f5e2b84a
itsok <vsudhanshu18@gmail.com>
2021-06-25 21:16:20 +0530

    day 1

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
-import React from "react";
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <h1>hi</h1>
-    </div>
+    <Switch>
+      <Route exact path="/">
+        This is my home page
+      </Route>
+
+      <Route exact path="/start">
+        This is my start
+      </Route>
+
+      <Route>This is 404 page</Route>
+    </Switch>
   );
 }
 
 export default App;
-
